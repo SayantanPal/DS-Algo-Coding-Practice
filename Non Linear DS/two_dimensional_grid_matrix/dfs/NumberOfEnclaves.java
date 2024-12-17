3ff30a0c7e46b4d22853d24986e73c96b1b90897
i524773 <sayantan.pal01@sap.com>
2024-12-18 00:42:45 +0530

    add code

--- Non Linear DS/two_dimensional_grid_matrix/dfs/NumberOfEnclaves.java
+++ Non Linear DS/two_dimensional_grid_matrix/dfs/NumberOfEnclaves.java
@@ -22,6 +22,20 @@ public class NumberOfEnclaves {
         }
     }
 
+    public void depthFirstSearch_v2(int[][] grid, int currRow, int currCol, int n, int m){
+
+        int[] X = {0, 0, 1, -1};
+        int[] Y = {1, -1, 0, 0};
+        for(int i = 0; i < 4; i++){
+            int nextRow = currRow + X[i];
+            int nextCol = currCol + Y[i];
+            if(validTraversal(nextRow, nextCol, n, m) && grid[nextRow][nextCol] == 1){
+                grid[nextRow][nextCol] = 0;
+                depthFirstSearch_v2(grid, nextRow, nextCol, n, m);
+            }
+        }
+    }
+
     public int numEnclaves(int[][] grid) {
 
         int n = grid.length;
@@ -66,5 +80,49 @@ public class NumberOfEnclaves {
         }
 
         return countOfEnclave;
+    }
+
+    public int numEnclaves_v2(int[][] grid) {
+
+        int n = grid.length;
+        int m = 0;
+        if(n > 0) m = grid[0].length;
+
+
+        for(int i = 0; i < n; i++){
+
+            if(grid[i][0] == 1){
+                grid[i][0] = 0;
+                depthFirstSearch_v2(grid, i, 0, n, m);
+            }
+
+            if(grid[i][m - 1] == 1){
+                grid[i][m - 1] = 0;
+                depthFirstSearch_v2(grid, i, m - 1, n, m);
+            }
+        }
+
+        for(int j = 0; j < m ;j++){
+            if(grid[0][j] == 1){
+                grid[0][j] = 0;
+                depthFirstSearch_v2(grid, 0, j, n, m);
+            }
+
+            if(grid[n - 1][j] == 1){
+                grid[n - 1][j] = 0;
+                depthFirstSearch_v2(grid, n - 1, j, n, m);
+            }
+        }
+
+        int countOfEnclave = 0;
+
+        for(int i = 1; i < n; i++){
+            for(int j = 1; j < m; j++){
+                countOfEnclave += grid[i][j];
+            }
+        }
+
+        return countOfEnclave;
+
     }
 }

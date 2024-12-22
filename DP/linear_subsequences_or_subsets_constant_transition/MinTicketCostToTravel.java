791e27a237c2719dbf1452e8bd41e9e3252f422d
i524773 <sayantan.pal01@sap.com>
2024-12-22 15:51:02 +0530

    add code

--- DP/linear_subsequences_or_subsets_constant_transition/MinTicketCostToTravel.java
+++ DP/linear_subsequences_or_subsets_constant_transition/MinTicketCostToTravel.java
@@ -1,6 +1,8 @@
 package linear_subsequences_or_subsets_constant_transition;
 
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 
 /*
 * Given a sorted array arr[] consisting of N positive integers such that arr[i] represent the days in which a worker will work and
@@ -12,7 +14,8 @@ import java.util.Arrays;
 // Link: https://leetcode.com/problems/minimum-cost-for-tickets/description/
 // Note: The days array is sorted
 // Note: The cost of plans are of constant values and applicable for all booking days
-// Note: Booking on some ith day also includes travelling on ith day within valid travel period
+// Note: Booking on some ith day also includes travelling on ith day within valid travel period.
+// So, validity of pass starts from booking day itself
 // Note: Booking happens prior to travel. So, Booking pointer will be always behind the travel pointer
 // Note: Goal is to find for the last travel day, not the last booking day
 class MinTicketCostToTravel {
@@ -121,7 +124,6 @@ class MinTicketCostToTravel {
         return dp[currTravelDay] = Math.min(option1, Math.min(option2, option3));
     }
 
-
     public static int minCostTickets(int[] days, int[] costs) {
         int[] travelOrNotDays = new int[366];
         for(int day: days){
@@ -166,8 +168,79 @@ class MinTicketCostToTravel {
         }
 
         return dp[lastTravelDay];
-
     }
 
+    public static int minimumCoins_v2(int n, int[] days, int[] cost) {
+
+        // min cost will store the minimum coins required to complete the whole trip.
+        int minCost = 0;
+
+        // Declaring two queues.
+        Queue<int[]> monthpass = new LinkedList<>();
+        Queue<int[]> weekpass = new LinkedList<>();
+
+        // No need to track 1 day pass because the 1-day booking pass applies for that same day travel only
+
+        for (int currTravelDay : days) {
+
+            // when current travel day has crossed the deadline,
+            // Removing expired days/passes from both the queues
+            // <= check instead of only < because last booking day also counted as the valid travel day
+            // Removing expired passes from the queue.
+
+            int minCostOfLastBookingDayForMonthlyPass = -1;
+
+            while (!monthpass.isEmpty()) {
+                int lastBookingDayForMonthlyPass = monthpass.peek()[0]; // first probable monthly pass booking day
+                int deadLineForlastBookingDayForMonthlyPass = lastBookingDayForMonthlyPass + 30; // deadline of 30 day validity period from that pass booking day
+                // 30th day is not covered within 30 day validity period
+                if(deadLineForlastBookingDayForMonthlyPass <= currTravelDay){ // if current travel day crossed validity period of last monthly pass booking day
+                    monthpass.poll(); // expire that monthly pass by removing its booking day
+                } else { // otherwise current travel day is within the validity period of its booking day
+                    minCostOfLastBookingDayForMonthlyPass = monthpass.peek()[1]; // consider today's travel day booking on that booking day for within 30 days validity
+                    break;
+                }
+            }
+
+            int minCostOfLastBookingDayForWeeklyPass = -1;
+            while (!weekpass.isEmpty()) {
+                int lastBookingDayForWeeklyPass = weekpass.peek()[0]; // weekly pass booking day
+                int deadLineForlastBookingDayForWeeklyPass = lastBookingDayForWeeklyPass + 7; // deadline of 7 day validity period from that pass booking day
+                // 7th day is not covered within 7 day validity period
+                if(deadLineForlastBookingDayForWeeklyPass <= currTravelDay){ // if current travel day crossed validity period of last weekly pass booking day
+                    weekpass.poll(); // expire that weekly pass by removing its booking day
+                } else { // otherwise current travel day is within the validity period of its booking day
+                    minCostOfLastBookingDayForWeeklyPass = weekpass.peek()[1]; // consider today's travel day booking on that booking day for within 7 days validity
+                    break;
+                }
+            }
+
+            // Buying 30-day and 7-day pass on current day.
+            int currBookingDayForNextTravelDay = currTravelDay;
+            int costOf1DayPassForCurrTravelDay = minCost + cost[0];
+
+            // If there exists no previous travel day whose 7-day or 40-day validity can cover today's current travel day
+
+            // If no valid 7-day pass, take the current 7-day cost
+            int costOf7DayPassBookedOnCurrTravelDay = minCost + cost[1];
+            // when there exists no such prior booking date whose 7-day validity period covers current day
+            if(weekpass.isEmpty()) //if(minCostOfLastBookingDayForWeeklyPass == -1)
+                minCostOfLastBookingDayForWeeklyPass = costOf7DayPassBookedOnCurrTravelDay; // since travel on current day mandatory hence, consider today's 7-day booking pass
+
+            weekpass.add(new int[]{currBookingDayForNextTravelDay, costOf7DayPassBookedOnCurrTravelDay});
+
+            // If no valid 30-day pass, take the current 30-day cost
+            int costOf30DayPassBookedOnCurrTravelDay = minCost + cost[2];
+            // when there exists no such prior booking date whose 30-day validity period covers current day
+            if(monthpass.isEmpty()) // if(minCostOfLastBookingDayForMonthlyPass == -1)
+                minCostOfLastBookingDayForMonthlyPass = costOf30DayPassBookedOnCurrTravelDay; // since travel on current day mandatory hence, consider today's 30-day booking pass
+            monthpass.add(new int[]{currBookingDayForNextTravelDay, costOf30DayPassBookedOnCurrTravelDay});
+
+            // Updating minCost as min of today's travel with today's 1 day pass booking day, or (any prior day whose pass booking covers today as within valid day and if not, then today's 7-day and 30-day pass booking day)
+            minCost = Math.min(costOf1DayPassForCurrTravelDay, Math.min(minCostOfLastBookingDayForWeeklyPass, minCostOfLastBookingDayForMonthlyPass));
+        }
+
+        return minCost;
+    }
 
 }
---
+++ Pair.java
@@ -0,0 +1,11 @@
+public class Pair {
+    public int first;
+    public int second;
+
+    public Pair() {};
+
+    public Pair(int first, int second) {
+        this.first = first;
+        this.second = second;
+    }
+}

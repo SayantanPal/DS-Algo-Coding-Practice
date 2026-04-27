7fbf742f1318eec2217166c56a5e8fb549ff37d7
i524773 <sayantan.pal01@sap.com>
2026-04-27 10:03:17 +0530

    add code

---
+++ Linear DS/Hashing/hashmap/TopKFreqElements.java
@@ -0,0 +1,53 @@
+package hashmap;
+import java.util.*;
+
+// Link: https://leetcode.com/problems/top-k-frequent-elements/
+public class TopKFreqElements {
+
+    public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> freqLookUp = new HashMap<>();
+
+        // count frequency of all elements (key: num, val: occurance/freq count)
+        for(int num: nums){
+            freqLookUp.put(num, freqLookUp.getOrDefault(num, 0) + 1);
+        }
+
+
+        List<Integer>[] freqBucket = new List[nums.length + 1]; //array of lists
+
+        // for multiple elements having same occurance,
+        // reverse lookup map (index: occurance count, val: multiple num having same occurance count)
+        // which is bucket of frequency counts of multiple numbers
+        for(int num: freqLookUp.keySet()){
+            int occurance = freqLookUp.get(num);
+            if(freqBucket[occurance] == null){
+                freqBucket[occurance] = new ArrayList<>();
+            }
+            freqBucket[occurance].add(num);
+        }
+
+        // top k frequent elements only
+        int result[] = new int[k];
+        int l = 0;
+
+        // since top K (i.e., in descending order of freq, first k elements)
+        // hence, scan in reverse order pass
+        for(int freqCount = freqBucket.length - 1; freqCount >= 1; freqCount--){
+            // if null, then there is no number with that freq count
+            if(freqBucket[freqCount] != null){
+                // for each freq count bucket, there can be multiple numbers with same freq count
+                for(int j = 0; j < freqBucket[freqCount].size(); j++){
+                    result[l++] = freqBucket[freqCount].get(j);
+                    if(l == k){
+                        break;
+                    }
+                }
+                if(l == k){
+                    break;
+                }
+            }
+        }
+
+        return result;
+    }
+}

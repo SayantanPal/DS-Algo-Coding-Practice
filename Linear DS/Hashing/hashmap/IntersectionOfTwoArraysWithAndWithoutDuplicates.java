e6861fbf6a6ff1a8f1a03b22f682a2526499cb03
i524773 <sayantan.pal01@sap.com>
2026-05-19 00:17:03 +0530

    add code

--- Linear DS/Hashing/hashmap/IntersectionOfTwoArraysWithAndWithoutDuplicates.java
+++ Linear DS/Hashing/hashmap/IntersectionOfTwoArraysWithAndWithoutDuplicates.java
@@ -5,10 +5,9 @@ import java.util.HashMap;
 import java.util.List;
 
 // Link: https://leetcode.com/problems/intersection-of-two-arrays-ii/
-public class IntersectionOfTwoArraysWithDuplicates {
+public class IntersectionOfTwoArraysWithAndWithoutDuplicates {
 
-
-    public int[] intersect(int[] nums1, int[] nums2) {
+    public int[] intersectWithDuplicates(int[] nums1, int[] nums2) {
         HashMap<Integer, Integer> lookUp = new HashMap<>();
         for(int i: nums1){
             lookUp.put(i, lookUp.getOrDefault(i, 0) + 1);
@@ -32,7 +31,8 @@ public class IntersectionOfTwoArraysWithDuplicates {
 
     // since input nos are in bounded range from 0 to 1000,
     // replacing hashmap with freq array is faster
-    public int[] intersect_optimized(int[] nums1, int[] nums2) {
+    // Link: https://leetcode.com/problems/intersection-of-two-arrays-ii/
+    public int[] intersectWithDuplicates_optimized(int[] nums1, int[] nums2) {
         int[] lookUp = new int[1001]; //hashmap<Integer, Integer> lookUp = new hashmap<>();
         for(int i: nums1){
             lookUp[i]++; // lookUp.put(i, lookUp.getOrDefault(i, 0) + 1);
@@ -52,5 +52,29 @@ public class IntersectionOfTwoArraysWithDuplicates {
             result2[i] = result.get(i);
         }
         return result2;
+    }
+
+    // Link: https://leetcode.com/problems/intersection-of-two-arrays/
+    public int[] intersectionWithoutDuplicates(int[] nums1, int[] nums2) {
+        // Set<Integer> lookUp = new HashSet<>();
+        boolean[] lookUp = new boolean[1001];
+        for(int i: nums1){
+            lookUp[i] = true;// lookUp.add(i);
+        }
+
+        List<Integer> result = new ArrayList<>();
+        for(int i: nums2){
+            if(lookUp[i]){//if(lookUp.contains(i)){
+                result.add(i);
+                lookUp[i] = false;// lookUp.remove(i);
+            }
+        }
+
+        int[] result2 = new int[result.size()];
+        for(int i = 0; i < result2.length; i++){
+            result2[i] = result.get(i);
+        }
+
+        return result2; // return result.stream().mapToInt(i -> i).toArray();
     }
 }

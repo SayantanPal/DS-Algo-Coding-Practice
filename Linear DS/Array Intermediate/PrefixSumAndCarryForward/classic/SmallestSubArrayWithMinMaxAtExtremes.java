7791d60dd28c06df363afaa9b2817b8a8d9cdb9b
i524773 <sayantan.pal01@sap.com>
2026-06-10 01:25:07 +0530

    add code

--- Linear DS/Array Intermediate/PrefixSumAndCarryForward/classic/SmallestSubArrayWithMinMaxAtExtremes.java
+++ Linear DS/Array Intermediate/PrefixSumAndCarryForward/classic/SmallestSubArrayWithMinMaxAtExtremes.java
@@ -1,8 +1,8 @@
 package classic;
 
 /*
+Problem: Closest Min Max
 * Given an array A, find the size of the smallest subarray such that it contains at least one occurrence of the maximum value of the array and at least one occurrence of the minimum value of the array.
-*
 * */
 public class SmallestSubArrayWithMinMaxAtExtremes {
 

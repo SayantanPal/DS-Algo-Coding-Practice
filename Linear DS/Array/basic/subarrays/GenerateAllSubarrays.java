f0c57da2bc95dbe8dd4f86a1563b656a5defd144
i524773 <sayantan.pal01@sap.com>
2026-06-10 01:19:01 +0530

    add code

--- Linear DS/Array/basic/subarrays/GenerateAllSubarrays.java
+++ Linear DS/Array/basic/subarrays/GenerateAllSubarrays.java
@@ -4,6 +4,13 @@ package basic.subarrays;
 * Return a 2D array consisting of all the subarrays of the array
 * Note : The order of the subarrays in the resulting 2D array does not matter.
 * */
+// Total No of subarrays is given by:
+// For i = 0th element, it is N subarrays
+// For i = 1st element, it is N - 1 subarrays
+// For i = 2nd element, it is N - 2 subarrays
+// For i = (N - 1)th element, it is 1 subarray
+// So total count of subarrays for all indexes = N*(N+1)/2
+// For j = (N - i)th element, it is (N - i) subarrays
 public class GenerateAllSubarrays {
     public int[][] solve(int[] A) {
         int n = A.length;

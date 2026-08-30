25df14ff7272bca34a51f2ecc84c6bb3026f821e
i524773 <sayantan.pal01@sap.com>
2026-08-30 23:56:14 +0530

    add code

--- Linear DS/Array Intermediate/PrefixSumAndCarryForward/prodarraywithoutcurrelem/ProdOfArrayExceptCurrentIndex.java
+++ Linear DS/Array Intermediate/PrefixSumAndCarryForward/prodarraywithoutcurrelem/ProdOfArrayExceptCurrentIndex.java
@@ -2,9 +2,27 @@ package prodarraywithoutcurrelem;
 
 import java.util.Arrays;
 
+// Link: https://leetcode.com/problems/product-of-array-except-self/description/
 public class ProdOfArrayExceptCurrentIndex {
 
-    public int[] productExceptSelf(int[] nums) {
+    public int[] productExceptSelf(int[] A) {
+        int n = A.length;
+        int[] prefixProd = new int[n];
+        int[] suffixProd = new int[n];
+        prefixProd[0] = 1;
+        suffixProd[n - 1] = 1;
+        for(int i = 1; i < n; i++){
+            prefixProd[i] = prefixProd[i - 1] * A[i - 1];
+            suffixProd[n - 1 - i] = suffixProd[n - i] * A[n - i];
+        }
+        int[] result = new int[n];
+        for(int i = 0; i < n; i++){
+            result[i] = prefixProd[i] * suffixProd[i];
+        }
+        return result;
+    }
+
+    public int[] productExceptSelf_v2(int[] nums) {
         int n = nums.length;
         if(n == 0) return nums;
 
@@ -15,8 +33,9 @@ public class ProdOfArrayExceptCurrentIndex {
 
         for(int i = 0; i < n; i++){
             prefixSuffix[i] *= prefix;
-            prefixSuffix[n-1-i] *= suffix;
             prefix *= nums[i];
+
+            prefixSuffix[n-1-i] *= suffix;
             suffix *= nums[n-1-i];
         }
 

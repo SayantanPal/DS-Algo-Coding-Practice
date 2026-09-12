803d1580cc1871c5be6d562ba85f6e4123ce2cca
i524773 <sayantan.pal01@sap.com>
2026-09-12 21:10:38 +0530

    add code

---
+++ Linear DS/Stack/immediatelargerorsmalleronleftorright/NextSmallerNoToTheImmediateLeft.java
@@ -0,0 +1,32 @@
+package immediatelargerorsmalleronleftorright;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.Stack;
+
+// Link:
+public class NextSmallerNoToTheImmediateLeft {
+
+    public ArrayList<Integer> next_smaller_number_to_the_left(ArrayList<Integer> nums) {
+        ArrayList<Integer> res = new ArrayList<>();
+        Stack<Integer> stack = new Stack<>();
+        // Initialize result list with zeros.
+        for (int i = 0; i < nums.size(); i++) {
+            res.add(0);
+        }
+        // Find the next largest number of each element, starting with the rightmost
+        // element.
+        for (int i = 0; i < nums.size(); i++) {
+            // Pop values from the top of the stack until the current value's next largest
+            // number is at the top.
+            while (!stack.isEmpty() && stack.peek() >= nums.get(i)) {
+                stack.pop();
+            }
+            // Record the current value's next largest number, which is at the top of the
+            // stack. If the stack is empty, record -1.
+            res.set(i, stack.isEmpty() ? -1 : stack.peek());
+            stack.push(nums.get(i));
+        }
+        return res;
+    }
+}
